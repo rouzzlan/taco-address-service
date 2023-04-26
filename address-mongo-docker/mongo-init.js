6b7a9766e2000c0d74e9ee0654a3465921181b4e
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-04-26 15:08:18 +0200

    default values insert for docker mongodb

--- address-mongo-docker/mongo-init.js
+++ address-mongo-docker/mongo-init.js
@@ -10,3 +10,19 @@ db.createUser(
         ]
     }
 );
+
+db = new Mongo().getDB('addressdb');
+
+db.createCollection('Address');
+
+db.Address.insertMany([
+    {
+        "_id": "644874b3777555522fd294ca",
+        "city": "Emblem",
+        "country": "BE",
+        "hash": "c49a20095a4f5ac6f83a3c0ff33854327b1cf3c2c5b39aa6f934c1fe53c1dd61",
+        "state": "Antwerpen",
+        "street": "Kloosterstraat 27A",
+        "zip": "2520"
+    }
+]);
